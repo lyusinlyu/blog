afd3e6cd60746483a162db5be977deaea194b020
lus <lusinehovh@gmail.com>
2018-03-23 20:03:12 +0400

    DB_filling testing functions

--- routes/web.php
+++ routes/web.php
@@ -95,6 +95,97 @@ function dbDataFill() {
     Message::create($msg);
 }
 
+function dbDataFill2() {
+    /*
+    1) You have 3 users
+    2) Each of them have a category or categories
+    3) each of them have posts on their categories
+    4) each of them can comment either his own, or all other posts
+    5) each of them can like either his own, or all other posts
+    */
+    //Users input data
+    $userInputs = [
+        'user1' => [
+            'name' => 'Dean',
+            'surname' => 'Winchester',
+            'nickname' => 'Dean777',
+            'password' => 'bla',
+            'email' => 'dwinchester@gmail.com',
+            'gender' => 'male',
+            'date_of_birth' => '1978-03-03 03:00:00'
+        ],
+        'user2' => [
+            'name' => 'Sam',
+            'surname' => 'Winchester',
+            'nickname' => 'Sam666',
+            'password' => 'bla',
+            'email' => 'swinchester@gmail.com',
+            'gender' => 'male',
+            'date_of_birth' => '1982-07-17 00:03:00'
+        ],
+        'user3' => [
+            'name' => 'Robert',
+            'surname' => 'Singer',
+            'nickname' => 'Bobby123',
+            'password' => 'cheshireCat',
+            'email' => 'singer123@gmail.com',
+            'gender' => 'male',
+            'date_of_birth' => '1960-11-03 06:06:00'
+        ]
+    ];
+    foreach ($userInputs as $userInput) {
+        $user = User::create($userInput);
+        $usersIds[] = $user -> id;
+    }
+    $categoryInputs = [
+       1 => ['user_id' => $usersIds[0], 'title' => 'Werevolves'],
+       2 => ['user_id' => $usersIds[0], 'title' => 'Vampires'],
+       3 => ['user_id' => $usersIds[1], 'title' => 'Ghosts'],
+       4 => ['user_id' => $usersIds[2], 'title' => 'Shifters'],
+       5 => ['user_id' => $usersIds[2], 'title' => 'Demons'],
+       6 => ['user_id' => $usersIds[2], 'title' => 'Angels']
+    ];
+    foreach ($categoryInputs as $categoryInput) {
+        $category = Category::create($categoryInput);
+        $categoryIds[] = $category -> id;
+    }
+    $postsInputs = [
+        1 => ['user_id' => $usersIds[0], 'category_id' => $categoryIds[0], 'title' => 'Bloody Mary', 'content' => 'content'],
+        2 => ['user_id' => $usersIds[0], 'category_id' => $categoryIds[1], 'title' => 'Asylum', 'content' => 'content'],
+        3 => ['user_id' => $usersIds[1], 'category_id' => $categoryIds[2], 'title' => 'Shadow', 'content' => 'content'],
+        4 => ['user_id' => $usersIds[2], 'category_id' => $categoryIds[3], 'title' => 'Hell House', 'content' => 'content'],
+        5 => ['user_id' => $usersIds[2], 'category_id' => $categoryIds[3], 'title' => 'Dead Man\'s Blood', 'content' => 'content'],
+        6 => ['user_id' => $usersIds[0], 'category_id' => $categoryIds[1], 'title' => 'Family Remains', 'content' => 'content'],
+    ];
+    foreach ($postsInputs as $postInput) {
+        $post = Post::create($postInput);
+        $postsIds[] = $post -> id;
+    }
+    $commentsInputs = [
+        ['user_id' => $usersIds[0], 'post_id' => $postsIds[2], 'content' => 'Great'],
+        ['user_id' => $usersIds[1], 'post_id' => $postsIds[2], 'content' => 'Thanks'],
+        ['user_id' => $usersIds[1], 'post_id' => $postsIds[3], 'content' => 'Have Seen this lately)'],
+        ['user_id' => $usersIds[0], 'post_id' => $postsIds[3], 'content' => 'Yep. Me too'],
+        ['user_id' => $usersIds[2], 'post_id' => $postsIds[3], 'content' => 'Thanks for commenting'],
+        ['user_id' => $usersIds[2], 'post_id' => $postsIds[4], 'content' => 'new Post is here'],
+        ['user_id' => $usersIds[0], 'post_id' => $postsIds[5], 'content' => 'ok'],
+    ];
+    foreach ($commentsInputs as $commentsInput) {
+        Comment::insert($commentsInput);
+        }
+    $likes = [
+        ['user_id' => $usersIds[0], 'post_id' => $postsIds[2]],
+        ['user_id' => $usersIds[1], 'post_id' => $postsIds[1]],
+        ['user_id' => $usersIds[2], 'post_id' => $postsIds[0]],
+        ['user_id' => $usersIds[0], 'post_id' => $postsIds[3]],
+        ['user_id' => $usersIds[1], 'post_id' => $postsIds[4]],
+        ['user_id' => $usersIds[2], 'post_id' => $postsIds[5]]        
+    ];
+    foreach ($likes as $like) {
+        Like::insert($like);
+        }
+    }
+    
 
 
 /*
@@ -112,5 +203,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/test', function () {
-    dbDataFill();
+    // dbDataFill();
+    dbDataFill2();
 });
