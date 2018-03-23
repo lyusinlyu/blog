c3b1ccf151ae407a6afedc58053740a958c28ef4
lus <lusinehovh@gmail.com>
2018-03-23 15:01:26 +0400

    Inserting data into db fixed

--- routes/web.php
+++ routes/web.php
@@ -51,76 +51,41 @@ function dbDataFill() {
        2 => ['user_id' => $user1_id, 'title' => 'Category 2'],
        3 => ['user_id' => $user1_id, 'title' => 'Category 3']
     ];
-    //Creating Categories with insert()
+    //Creating Categories 
 
-    // $ids = [];
-
-    // foreach($category_inputs as $category_input)
-    // {
-    //     $ids[] = DB::table('categories')->insertGetId($category_input);
-    // }
-    // $cat1_id = $ids[0];
-    // $cat2_id = $ids[1];
-    // $cat3_id = $ids[2];  
+    foreach($category_inputs as $category_input)
+    {
+        $category = Category::create($category_input);
+        $ids[] = $category->id;
+    }
    
-
-    //Creating Categories with create()
-    $u1_cat1 = Category::create($category_inputs[1]);
-    $cat1_id = $u1_cat1 -> id;    
-
-    $u1_cat2 = Category::create($category_inputs[2]);
-    $cat2_id = $u1_cat2 -> id;    
-
-    $u1_cat3 = Category::create($category_inputs[3]);
-    $cat3_id = $u1_cat3 -> id;
-    
     //Creating Posts
-    //Category_1 Posts
+    
     $post_inputs = [
-        1 => ['user_id' => $user1_id, 'category_id' => $cat1_id, 'title' => 'Post_1 Title', 'content' => 'Post_1 Content'],
-        2 => ['user_id' => $user1_id, 'category_id' => $cat1_id, 'title' => 'Post_2 Title', 'content' => 'Post_2 Content'],
-        3 => ['user_id' => $user1_id, 'category_id' => $cat2_id, 'title' => 'Post_1 Title', 'content' => 'Post_1 Content'],
-        4 => ['user_id' => $user1_id, 'category_id' => $cat2_id, 'title' => 'Post_2 Title', 'content' => 'Post_2 Content'],
-        5 => ['user_id' => $user1_id, 'category_id' => $cat3_id, 'title' => 'Post_1 Title', 'content' => 'Post_1 Content'],
-        6 => ['user_id' => $user1_id, 'category_id' => $cat3_id, 'title' => 'Post_2 Title', 'content' => 'Post_2 Content']
+        ['user_id' => $user1_id, 'category_id' => $ids[0], 'title' => 'Post_1 Title', 'content' => 'Post_1 Content'],
+        ['user_id' => $user1_id, 'category_id' => $ids[0], 'title' => 'Post_2 Title', 'content' => 'Post_2 Content'],
+        ['user_id' => $user1_id, 'category_id' => $ids[1], 'title' => 'Post_1 Title', 'content' => 'Post_1 Content'],
+        ['user_id' => $user1_id, 'category_id' => $ids[1], 'title' => 'Post_2 Title', 'content' => 'Post_2 Content'],
+        ['user_id' => $user1_id, 'category_id' => $ids[2], 'title' => 'Post_1 Title', 'content' => 'Post_1 Content'],
+        ['user_id' => $user1_id, 'category_id' => $ids[2], 'title' => 'Post_2 Title', 'content' => 'Post_2 Content']
 
     ];
-    $cat1_post1 = Post::create($post_inputs[1]);
-    $cat1_post1_id = $cat1_post1->id;
-
-    $cat1_post2 = Post::create($post_inputs[2]);
-    $cat1_post2_id = $cat1_post2->id;
-
-    //Category_2 Posts
-    $cat2_post1 = Post::create($post_inputs[3]);
-    $cat2_post1_id = $cat2_post1->id;
-
-    $cat2_post2 = Post::create($post_inputs[4]);
-    $cat2_post2_id = $cat2_post2->id;
+    $postIds = [];
+    for ($i = 0; $i < count($post_inputs); $i++) {
+        $post = Post::create($post_inputs[$i]);
+        $postIds[] = $post->id;
+    }
     
-    //Category_3 Posts
-    $cat3_post1 = Post::create($post_inputs[5]);
-    $cat3_post1_id = $cat3_post1->id;
-
-    $cat3_post2 = Post::create($post_inputs[6]);
-    $cat3_post2_id = $cat3_post2->id;
-
-
     //Creating User_2
     $user2 = User::create($user_inputs['user_2']);
     $user2_id = $user2->id;
 
     //Likes
-    $likes = [
-        ['user_id' => $user2_id, 'post_id' => $cat1_post1_id],
-        ['user_id' => $user2_id, 'post_id' => $cat1_post2_id],
-        ['user_id' => $user2_id, 'post_id' => $cat2_post1_id],
-        ['user_id' => $user2_id, 'post_id' => $cat2_post2_id],
-        ['user_id' => $user2_id, 'post_id' => $cat3_post1_id],
-        ['user_id' => $user2_id, 'post_id' => $cat3_post2_id]
-    ];
    
-    DB::table('likes')->insert($likes);
+    for ($i = 0; $i < count($postIds); $i++) {
+        $likes[] = ['user_id' => $user2_id, 'post_id' => $postIds[$i]];
+    }
+    Like::insert($likes);
     
 
     //Message 
