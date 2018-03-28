3ccefda36553f944f3273cb7d439cf226b2bfe2a
lus <lusinehovh@gmail.com>
2018-03-28 20:05:28 +0400

    user by identificator param

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -7,7 +7,7 @@ use App\Models\User;
 
 class UsersController extends Controller
 {
-    public function showUsers()
+    public function showUsers(Request $request)
     {
         $users = User::all();
         if($users) {
@@ -18,9 +18,34 @@ class UsersController extends Controller
 
     public function showUser(Request $request, $id)
     {
-        $user = User::where('id', '=', $id)->first();
-        if($user) {
-            dd($user);
+        if($request->query('identificator') === 'email') {
+            $email = $id;
+            if($email) {
+            $user = User::where('email', $email)->first();
+            if($user) {
+                dd($user);
+            }
+            // $user = User::create(
+            //     [
+            //         'name' => 'unknown',
+            //         'surname' => 'unknown',
+            //         'nickname' => 'unknown',
+            //         'password' => 'default_pass',
+            //         'email' => $email,
+            //         'gender' => 'unknown',
+            //         'date_of_birth' => '1970-12-12 00:00:00'
+            //     ]
+            // );
+            // dd($user);
+
         }
+        } else {
+            $user = User::where('id', $id)->first();
+            if($user) {
+            dd($user);
+            }
+        }
+
     }
+
 }
