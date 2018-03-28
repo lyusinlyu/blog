d33231116aafb47098cd13988161c230b1b42e1d
lus <lusinehovh@gmail.com>
2018-03-28 11:46:46 +0400

    Added route groups

--- routes/web.php
+++ routes/web.php
@@ -4,50 +4,62 @@ use App\Models\{
     User
 };
 
-Route::get('/users', 'UsersController@showUsers');
-Route::get('/users/{user_id}', 'UsersController@showUser');
-Route::get('/users/{user_id}/posts', 'PostsController@showPosts');
-Route::get('/users/{user_id}/posts/{post_id}', 'PostsController@showPost');
-Route::get('users/{user_id}/categories', 'CategoriesController@showCategories');
-Route::get('users/{user_id}/categories/{category_id}', 'CategoriesController@showCategory');
+Route::group(['prefix' => 'users'], function(){
+    Route::get('/', 'UsersController@showUsers');
+    Route::get('{user_id}', 'UsersController@showUser');
+    Route::get('{user_id}/posts', 'PostsController@showPosts');
+    Route::get('{user_id}/posts/{post_id}', 'PostsController@showPost');
+    Route::get('{user_id}/categories', 'CategoriesController@showCategories');
+    Route::get('{user_id}/categories/{category_id}', 'CategoriesController@showCategory');
+});
 
-Route::get('callback/users', function() {
-    $users = User::all();
+
+
+Route::group(['prefix' => 'callback/users'], function(){
+    Route::get('/', function() {
+        $users = User::all();
         if($users) {
             dd($users);
         }
-});
-Route::get('callback/users/{user_id}', function($id) {
-    $user = User::where('id', '=', $id)->first();
+    });
+    Route::get('{user_id}', function($id) {
+        $user = User::where('id', '=', $id)->first();
         if($user) {
             dd($user);
         }
-});
-Route::get('callback/users/{user_id}/posts', function($user_id) {
-    $user = User::where('id', $user_id)->first();
+    });
+    Route::get('{user_id}/posts', function($user_id) {
+        $user = User::where('id', $user_id)->first();
         if($user) {
             $posts = $user->posts;
             dd($posts);
         }
-});
-Route::get('callback/users/{user_id}/posts/{post_id}', function($user_id, $post_id) {
-    $user = User::where('id', $user_id)->first();
+    });
+    Route::get('{user_id}/posts/{post_id}', function($user_id, $post_id) {
+        $user = User::where('id', $user_id)->first();
         if($user) {
             $post = $user->posts()->where('id', $post_id)->first();
             dd($post);
         }
-});
-Route::get('callback/users/{user_id}/categories', function($user_id) {
-    $user = User::where('id', $user_id)->first();
+    });
+    Route::get('{user_id}/categories', function($user_id) {
+        $user = User::where('id', $user_id)->first();
         if($user) {
             $categories = $user->categories;
             dd($categories);
         }
-});
-Route::get('callback/users/{user_id}/categories/{category_id}', function($user_id, $category_id) {
-    $user = User::where('id', $user_id)->first();
+    });
+    Route::get('{user_id}/categories/{category_id}', function($user_id, $category_id) {
+        $user = User::where('id', $user_id)->first();
         if($user) {
             $category = $user->categories()->where('id', $category_id)->first();
             dd($category);
         }
+    });
 });
+
+
+
+
+
+
